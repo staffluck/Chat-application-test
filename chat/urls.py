de8743533f19598fa54f3f0b9223d15add40fe1c
AgentDaun <agent47daun@gmailcom>
2021-07-01 17:41:42 +0400

    chat template

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dialogs/', views.DialogListCreateView.as_view()),
-    path('<str:room_name>/', views.room, name='room'),
 ]
