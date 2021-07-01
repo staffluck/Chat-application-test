f351578a79704dbb876506c7a5dc0d3eb815dab1
AgentDaun <agent47daun@gmailcom>
2021-07-01 16:09:11 +0400

    init chat app

---
+++ chat/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Dialog(models.Model):
+    users = models.ManyToManyField(User, related_name="dialogs", db_index=True)
+    title = models.CharField(max_length=60, null=True)
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="messages", db_index=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False, db_index=True)
+    body = models.TextField()
+    dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE, related_name="messages")
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.body)
+
+    def save(self, *args, **kwargs):
+        is_new = self.id
+        self.body = self.body.strip()
+        super().save(*args, **kwargs)
+        if is_new:
+            pass
---
+++ chat/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+
+
+
+def index(request):
+    return render(request, 'chat/index.html')
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
